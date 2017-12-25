a50f8c59325f4019fcc59e6e7f4c91952429169d
Kevin Harrington <mad.hephaestus@gmail.com>
2017-12-25 16:58:40 -0500

    parametric cone size

--- nugJug.groovy
+++ nugJug.groovy
@@ -2,16 +2,18 @@
 
 double baseThickness = 5
 double widthOfScoop = 200
-double opening = 25
+double opening =45
 //create an extruded polygon
 CSG polygon = Extrude.points(new Vector3d(0, 0, widthOfScoop),// This is the  extrusion depth
                 new Vector3d(0,0),// All values after this are the points in the polygon
                 new Vector3d(widthOfScoop,0),// Bottom right corner
                 new Vector3d(widthOfScoop,baseThickness),// upper right corner
+                 new Vector3d(widthOfScoop-10,baseThickness-2),// upper right corner
+                 new Vector3d(30,baseThickness-2),// upper right corner
                 new Vector3d(20,baseThickness)// upper left corner
         ).rotx(-90)
         .toYMin()
-
+//return polygon
 CSG hole =new Cylinder(opening-baseThickness,opening-baseThickness,40,(int)12).toCSG() // a one line Cylinder
 			
 CSG simpleSyntax =new Cylinder(opening,opening,40,(int)12).toCSG() // a one line Cylinder
@@ -50,17 +52,18 @@ CSG handle =new Cylinder(15,15,100,(int)12).toCSG() // a one line Cylinder
 			.movez(widthOfScoop/2+opening)
 			.movex(widthOfScoop/3)
 			.difference(cone.hull())
-import eu.mihosoft.vrl.v3d.*;
-import javafx.scene.text.Font;
 
 Font font = new Font("Arial",  30);
 
+double rotationAngle = Math.toDegrees(Math.atan2(widthOfScoop,(widthOfScoop/2)-opening))-90
+
 CSG text =CSG.unionAll( TextExtrude.text((double)5.0," NugJug",font))
+	.toZMin()
 	.rotx(90)
 	.toZMin()
 	.movey(1)
-	.rotz(-20)
-	.movez(10)
+	.rotz(rotationAngle)
+	.movez(opening/2+4)
 	
  
 return CSG.unionAll([scoopSecion,simpleSyntax,cone,handle,text])
